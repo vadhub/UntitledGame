7d7ee52151257ab530f17b54c7817906b89b99f4
ivanchenkogleb-ui <iwanschekogleb@gmail.com>
2026-02-26 19:51:39 +0300

    Spawnobject

--- Engine.java
+++ Engine.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Engine {
 
-    private int screenWidth = 800;
-    private int screenHeight = 600;
+public class Engine {
+    private float spawnTimer = 0f;
+
+    private int screenWidth ;
+    private int screenHeight ;
     private final List<GameObject> objects = new ArrayList<>();
     private final Random random = new Random();
 
@@ -15,28 +17,100 @@ public class Engine {
         objects.add(new GameObject(1, 0, 200, 30, 4, Color.BLUE));
         objects.add(new GameObject(2, 0, 300, 60, 1, Color.GREEN));
     }
+    public void setScreenSize(int width, int height) {
+        this.screenWidth = width;
+        this.screenHeight = height;
+    }
 
     public void update(float deltaTime) {
-        for (GameObject obj : objects) {
-            obj.update();
         }
 
-        objects.removeIf(obj -> obj.getX() + obj.getSize() < 0 || obj.getY() < -obj.getSize());
+
+    public void draw(Graphics g) {
+
     }
 
     public void spawnObject() {
-        float y = random.nextFloat() * (screenHeight - 60);
-        Color color = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat());
+        int type = random.nextInt(3);
 
-        GameObject obj = new GameObject(
-                random.nextInt(3, 100),
-                screenWidth,
-                y,
-                40,
-                2.5f,
-                color
-        );
-        objects.add(obj);
+        int x;
+        int y;
+
+        GameObject newObject;
+
+        if (type == 0) {
+            x = random.nextInt(screenWidth - 50);
+            y = random.nextInt(screenHeight - 50);
+            newObject = new GameObject(0, x, y, 50, 2, Color.RED);
+
+        } else if (type == 1) {
+            x = random.nextInt(screenWidth - 30);
+            y = random.nextInt(screenHeight - 30);
+            newObject = new GameObject(1, x, y, 30, 4, Color.BLUE);
+
+        } else {
+            x = random.nextInt(screenWidth - 60);
+            y = random.nextInt(screenHeight - 60);
+            newObject = new GameObject(2, x, y, 60, 1, Color.GREEN);
+        }
+
+        synchronized (objects) {
+            objects.add(newObject);
+        }
+
+        System.out.println("Объект типа " + newObject.getId() + " заспавнен");
+    }
+
+    public void spawnObject(GameObject gameObject) {
+        
+    }
+
+    public boolean collisionAABB(GameObject a, GameObject b) {
+        return false;
+    }
+
+    public void spawnObjectPattern(List<GameObject> pattern, long delay) {
+        Thread spawnThread = new Thread(new Runnable() { // создаёт новый поток
+            @Override
+            public void run() {
+                for (int i = 0; i < pattern.size(); i++) {
+                    GameObject elem = pattern.get(i);
+
+                    // копия объекта создаётся
+                    GameObject newObject = new GameObject(
+                            elem.getId(),
+                            elem.getX(),
+                            elem.getY(),
+                            elem.getSize(),
+                            elem.getSpeed(),
+                            elem.getColor()
+                    );
+
+                    // она добавляется в список
+                    synchronized (objects) {
+                        objects.add(newObject);
+                        System.out.println("Объект " + newObject.getId() + " заспавнен");
+                    }
+
+                    // задержка (очередь)
+                    if (i < pattern.size() - 1) {
+                        try {
+                            Thread.sleep(delay);
+                        } catch (InterruptedException e) {
+                            System.out.println("Спавн прерван!");
+                            break;
+                        }
+                    }
+                }
+                System.out.println("Общий спавн завершен");
+            }
+        });
+
+        spawnThread.start(); // запуск потока
+    }
+
+    // supplier of Pythagoras
+    public void moveTowards(GameObject attaker, GameObject target) {
     }
 
     public List<GameObject> getObjects() {
